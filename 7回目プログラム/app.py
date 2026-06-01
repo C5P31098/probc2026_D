f8f285f041a4a92a4baf77ff182f99a269a01638
瀬沼玲音 <c5p31053@bunkyo.ac.jp>
2026-06-01 15:22:43 +0900

    リポジトリ作成

--- 7回目プログラム/app.py
+++ 7回目プログラム/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request, redirect ,jsonify
+from flask import Flask, render_template, request, redirect, jsonify
 from model import init_db
 from route import create_route
 
@@ -7,15 +7,6 @@ def create_app():
     init_db(app)
     create_route(app)
     return app
-
-@app.route('/req_item', methods=['GET', 'POST'])
-def req_item():
-    if request.method == 'POST':
-        # フォームからデータが送られてきた時の処理
-        return redirect('/')
-    
-    # 画面を表示する処理
-    return render_template('req_item.html')
 
 if __name__ == "__main__":
     app = create_app()
